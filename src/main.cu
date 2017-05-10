fbd3afd940faa2086afc8ed0a0c9afbfad1a78db
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2017-05-10 17:45:44 +0100

    Enable command line setting of number of threads and blocks

--- src/main.cu
+++ src/main.cu
@@ -17,13 +17,17 @@ int main(int argc, char* argv[]) {
 
     string patternFile = "inputs/pattern_groups.bin";
     string eventFile = "inputs/single_track_hit_events.bin";
+    int nThreads = 64;
+    int nBlocks = 0;
     char opt;
 
-    while((opt = getopt(argc,argv,"e:p:")) != -1) {
+    while((opt = getopt(argc,argv,"e:p:t:b:")) != -1) {
         switch(opt)
         {
             case 'e': eventFile = optarg; break;
             case 'p': patternFile = optarg; break;
+            case 't': nThreads = atoi(optarg); break;
+            case 'b': nBlocks = atoi(optarg); break;
             default: cerr << "Invalid argument" << endl; exit(EXIT_FAILURE);
         }
     }
@@ -48,14 +52,14 @@ int main(int argc, char* argv[]) {
     GpuContext ctx1;
     MatchResults gpuResults1;
     createGpuContext(p, e, ctx1);
-    runGpuMatching(p, e, ctx1, gpuResults1, 128, 1000);
+    runGpuMatching(p, e, ctx1, gpuResults1, nThreads, nBlocks);
     deleteGpuContext(ctx1);
 
     // Perform gpu setup and pattern matching by pattern block
     GpuContext ctx2;
     MatchResults gpuResults2;
     createGpuContext(p, e, ctx2);
-    runGpuMatching(p, e, ctx2, gpuResults2, 64);
+    runGpuMatching(p, e, ctx2, gpuResults2, nThreads);
     deleteGpuContext(ctx2);
 
     // Compare cpu/gpu results
